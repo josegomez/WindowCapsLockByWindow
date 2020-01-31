e78549a3a0451883a26024398a5b2a2caf9381fb
Jose C Gomez <jose@josecgomez.com>
2020-01-31 11:41:00 -0500

    Initial Commit

--- BrandonIsAWhinyBitch/Program.cs
+++ BrandonIsAWhinyBitch/Program.cs
@@ -16,7 +16,7 @@ namespace BrandonIsAWhinyBitch
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCaps());
+            Application.Run(new frmCaps()); 
         }
     }
 }
